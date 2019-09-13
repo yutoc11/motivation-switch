cb45c324117733f906101a8ee2aa007fc80d7689
yutoc11 <kikuchi-yuto@sagojo.link>
2019-09-14 08:02:39 +0900

    firebaseのfunctionを使えるように

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,19 @@
 const functions = require('firebase-functions');
 
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
 exports.bigben = functions.https.onRequest((req, res) => {
 
   // const SITEURL = "〇〇〇〇〇"
   // const TITLE = "〇〇〇〇〇"
   // const DESCRIPTION = "〇〇〇〇〇"
   // const IMAGE = `https://firebasestorage.googleapis.com/〇〇〇〇〇〇〇`
-  const ogpImage = 'https://firebasestorage.googleapis.com/v0/b/motivation-switch.appspot.com/o/test.png?alt=media&token=e4a0e1ce-92f4-4463-b94b-8580808f01b7`
+  const ogpImage = "https://firebasestorage.googleapis.com/v0/b/motivation-switch.appspot.com/o/test.png?alt=media&token=e4a0e1ce-92f4-4463-b94b-8580808f01b7"
 
   res.status(200).send(`<!doctype html>
     <head>
